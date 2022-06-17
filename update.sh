e4d66dc6b65dbe7f11cb1cd56d45663004c852ee
On Additive <102109813+OnAdditive@users.noreply.github.com>
2022-06-17 13:08:02 -0700

    Update SH Files for gitbash

---
+++ update.sh
@@ -0,0 +1,5 @@
+cd home/plural/MMSApp/views;
+sudo rm index.pug;
+sudo wget https://raw.githubusercontent.com/OnAdditive/MMS/main/views/index.pug;
+cd /home/plural/MMSApp/public/images;
+sudo wget https://raw.githubusercontent.com/OnAdditive/MMS/main/public/images/onadditivelogo.png;
